f21e521edc721b2195927c52a6fa7b083f49d62d
Peter <peter.gruber@revenupath.com>
2013-10-14 17:14:19 +0200

    add postgres database config

---
+++ project/Build.scala
@@ -0,0 +1,28 @@
+import sbt._
+import Keys._
+import play.Project._
+
+object ApplicationBuild extends Build {
+
+  val appName         = "audience-extender"
+  val appVersion      = "0.1"
+
+  val appDependencies = Seq(
+    javaCore,
+    javaJpa,
+
+    "org.springframework" % "spring-context" % "3.2.2.RELEASE",
+    "javax.inject" % "javax.inject" % "1",
+
+    "org.springframework.data" % "spring-data-jpa" % "1.3.2.RELEASE",
+    "org.hibernate" % "hibernate-entitymanager" % "3.6.10.Final",
+    "postgresql" % "postgresql" % "9.1-901.jdbc4",
+
+    "org.mockito" % "mockito-core" % "1.9.5" % "test"
+  )
+
+  val main = play.Project(appName, appVersion, appDependencies).settings(
+    ebeanEnabled := false
+  )
+
+}
