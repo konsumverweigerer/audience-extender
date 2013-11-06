1722a3493b2b560267ebf0b40eb0163b9499a530
Peter <peter.gruber@revenupath.com>
2013-11-06 15:38:18 +0100

    fix typo

--- conf/evolutions/default/2.sql
+++ conf/evolutions/default/2.sql
@@ -4,4 +4,4 @@ alter table admin add column email_confirm_token varchar(255);
 
 # --- !Downs
 
-alter table admin drop column emai_confirm_token;
+alter table admin drop column email_confirm_token;
